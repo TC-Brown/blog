95297127329cd98f5ba4212ed611ff4b326b793b
TC Brown <trrncbrown@gmail.com>
2026-07-27 13:13:22 -0500

    ud blog  and vlog layout

--- error/error.js
+++ error/error.js
@@ -1,8 +1,8 @@
 function home() { 
-    window.location.href = "../index.html"
-    return window.location.href
+    window.location.href = "../index.html";
+    return window.location.href;
 }
 
 function contact() {
-    window.location.href = "../templates/contact.html"
+    window.location.href = "../templates/contact.html";
 }
